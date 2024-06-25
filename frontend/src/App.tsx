e991cca9044d191ca98a3b170fffc87e432b100c
Yoana.Aneva <ioana.aneva@gmail.com>
2024-06-25 21:15:06 +0300

    Add path to contact form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ContactsList from './listContacts';
 import ContactDetails from './Contact';
 import ContactList from './listContacts';
-
+import { CreateContact } from './CreateContact';
 
 function App() {
   return  <Router>
             <Routes>
             <Route path="/" element={<ContactList/>} />
             <Route path="/contact-details/:id" element={<ContactDetails/>} /> 
+            <Route path="/create-contect" element={<CreateContact/>} />
             </Routes>
           </Router>
 }
-
 
 export default App;
