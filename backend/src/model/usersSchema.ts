ae18cc0ca4b13d6f68a020a88f528716d38ee644
Aleksandar Hristov <aleks_avgust@abv.bg>
2024-06-26 22:29:12 +0300

    Created User Schema.

---
+++ backend/src/model/usersSchema.ts
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema ({
+    username: { type: "string", required: true },
+    email: { type: "string", required: true },
+    authentication: {
+        password: { type: "string", required: true, select: false },
+        salt: { type: "string", select: false },
+    },
+}, { versionKey: false });
+
+export const UserModel = mongoose.model('User', userSchema);
+
+export const dbMethods = {
+  getUsers: () => UserModel.find(),
+  getUserByEmail: (email: string) => UserModel.findOne({ email: email }),
+  getUserByUsername: (username: string) => UserModel.findOne({ username: username }),
+  getUserById: (id: string) => UserModel.findById({ id: id }),
+  createUser: (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject()),
+  deleteUserById: (id: string) => UserModel.findOneAndDelete({_id: id}),
+  updateUserById: (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values),
+};
